286dae830b909ed30b59969c85ac31dacbbda5ba
andresusanto <andre@andresusanto.info>
2015-06-09 13:37:08 +0700

    Update struktur view

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -16,6 +16,7 @@ Route::get('/', 'WelcomeController@index');
 Route::get('home', 'HomeController@index');
 
 Route::controllers([
+	'entry' => 'EntryController',
 	'auth' => 'Auth\AuthController',
 	'password' => 'Auth\PasswordController',
 ]);
--- resources/views/konten/entry.blade.php
+++ resources/views/konten/entry.blade.php
@@ -1,6 +1,23 @@
 @extends('base')
 
 @section('content')
+<div class="row wrapper border-bottom white-bg page-heading">
+    <div class="col-lg-10">
+        <h2>Layouts</h2>
+        <ol class="breadcrumb">
+            <li>
+                <a href="index.html">Home</a>
+            </li>
+            <li class="active">
+                <strong>Layouts</strong>
+            </li>
+        </ol>
+    </div>
+    <div class="col-lg-2">
+
+    </div>
+</div>
+
 <div class="wrapper wrapper-content animated fadeInRight">   
 	<div class="row">
 		<div class="col-lg-12">
