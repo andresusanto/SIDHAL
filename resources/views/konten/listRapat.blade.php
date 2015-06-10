5345e7ddd9db1e0469e965c70aee185501630a4f
ndro <hendro.tb@gmail.com>
2015-06-10 10:33:14 +0700

    tabel rapat done

--- resources/views/konten/listRapat.blade.php
+++ resources/views/konten/listRapat.blade.php
@@ -13,62 +13,54 @@
 
 <script>
         $(document).ready(function () {
-
-            var mydata;
 			
-			$.getJSON("{{ action('DaftarRapatController@getData') }}", function(result){
-				$.each(result, function(i, field){
-					$("#test").append(field + " ");
+			$.getJSON( '{{ action("DaftarRapatController@getData") }}', function( mydata ) {
+            }).done(function(mydata){
+			
+				
+				function link(cellValue, options, rowdata, action)
+				{
+					return "<a href='" + rowdata.id + "'><span class='ui-icon ui-icon-folder-collapsed'>&nbsp;&nbsp;&nbsp;</span></a>";
+				}
+				
+				function link2(cellValue, options, rowdata, action)
+				{
+					return "<a href='" + rowdata.name + "'><span class='ui-icon ui-icon-circle-check'>&nbsp;&nbsp;&nbsp;&nbsp;</span></a>";
+				}
+
+				// Configuration for jqGrid Example 1
+				$("#table_list_1").jqGrid({
+					data: mydata,
+					datatype: "local",
+					height: 250,
+					autowidth: true,
+					shrinkToFit: true,
+					rowNum: 14,
+					rowList: [10, 20, 30],
+					colNames: ['Jenis Rapat', 'Waktu', 'Tempat', 'Pembahasan', 'Pimpinan', 'Aksi 1', 'Aksi 2'],
+					colModel: [
+						{name: 'jenis_rapat', index: 'jenis_rapat', width: 90},
+						{name: 'waktu', index: 'waktu', width: 100, sorttype: "date", formatter: "date"},
+						{name: 'tempat', index: 'tempat', width: 80},
+						{name: 'pembahasan', index: 'pembahasan', width: 80},
+						{name: 'pimpinan', index: 'pimpinan', width: 80 },
+						{name: 'aksi1', index: 'aksi1', width: 40,align: 'center',formatter: link},
+						{name: 'aksi1', index: 'aksi1', width: 40,align: 'center',formatter: link2}
+						
+					],
+					pager: "#pager_list_1",
+					viewrecords: true,
+					//caption: "Example jqGrid 1",
+					hidegrid: false
 				});
+
+				// Add responsive to jqGrid
+				$(window).bind('resize', function () {
+					var width = $('.jqGrid_wrapper').width();
+					$('#table_list_1').setGridWidth(width);
+				});
+				
 			});
-			
-			// [
-                // {id: "1", invdate: "2010-05-24", name: "test", note: "note", tax: "10.00", total: "2111.00"} ,
-                // {id: "29", invdate: "2007-09-01", name: "test3", note: "note3", amount: "400.00", tax: "30.00", total: "430.00"}
-            // ];
-			
-			function link(cellValue, options, rowdata, action)
-			{
-				return "<a href='" + rowdata.id + "'>+</a>";
-			}
-			
-			function link2(cellValue, options, rowdata, action)
-			{
-				return "<a href='" + rowdata.name + "'>-</a>";
-			}
-
-            // Configuration for jqGrid Example 1
-            $("#table_list_1").jqGrid({
-                data: mydata,
-                datatype: "local",
-                height: 250,
-                autowidth: true,
-                shrinkToFit: true,
-                rowNum: 14,
-                rowList: [10, 20, 30],
-                colNames: ['Inv No', 'Jenis Rapat', 'Waktu', 'Tempat', 'Pembahasan', 'Pimpinan', 'Aksi 1', 'Aksi 2'],
-                colModel: [
-                    {name: 'id', index: 'id', width: 60},
-                    {name: 'jenis_rapat', index: 'jenis_rapat', width: 90},
-                    {name: 'waktu', index: 'waktu', width: 100, sorttype: "date", formatter: "date"},
-                    {name: 'tempat', index: 'tempat', width: 80},
-                    {name: 'pembahasan', index: 'pembahasan', width: 80},
-                    {name: 'pimpinan', index: 'pimpinan', width: 80 },
-                    {name: 'aksi1', index: 'aksi1', width: 40,align: 'center',formatter: link},
-                    {name: 'aksi1', index: 'aksi1', width: 40,align: 'center',formatter: link2}
-                    
-                ],
-                pager: "#pager_list_1",
-                viewrecords: true,
-                //caption: "Example jqGrid 1",
-                hidegrid: false
-            });
-
-            // Add responsive to jqGrid
-            $(window).bind('resize', function () {
-                var width = $('.jqGrid_wrapper').width();
-                $('#table_list_1').setGridWidth(width);
-            });
         });
 
     </script>
