2f05c1f16218bd8eaa82efe825d5e197da99a858
ndro <hendro.tb@gmail.com>
2015-07-01 18:53:46 +0700

    perubahan result rapat

--- resources/views/konten/resultRapat.blade.php
+++ resources/views/konten/resultRapat.blade.php
@@ -3,26 +3,81 @@
 @section('addoncss')
 <link href="{{ asset('/css/plugins/jqGrid/ui.jqgrid.css') }}" rel="stylesheet">
 <link href="{{ asset('/css/plugins/jQueryUI/jquery-ui-1.10.4.custom.min.css') }}" rel="stylesheet">
+<link href="{{ asset('/css/plugins/toastr/toastr.min.css') }}" rel="stylesheet">
+<style>
+	th.ui-th-column div {
+		white-space: normal !important;
+		height: auto !important;
+		padding: 2px;
+	}
+</style>
 @endsection
 
 @section('addonjs')
 <!-- jqGrid -->
 <script src="{{ asset('/js/plugins/jqGrid/i18n/grid.locale-en.js') }}"></script>
 <script src="{{ asset('/js/plugins/jqGrid/jquery.jqGrid.min.js') }}"></script>
-<script src="{{ asset('/js/plugins/jqGrid/jquery.jqGrid.min.js') }}"></script>
+<script src="{{ asset('/js/plugins/toastr/toastr.min.js') }}"></script>
 <script>
         $(document).ready(function () {
+			@if(Session::has('update'))
+		
+				toastr.options = {
+					closeButton: true,
+					progressBar: true,
+					showMethod: 'slideDown',
+					timeOut: 3000
+				};
+				toastr.success('Rapat berhasil diubah.');
+
+			@endif
+			@if(Session::has('delete'))
+		
+				toastr.options = {
+					closeButton: true,
+					progressBar: true,
+					showMethod: 'slideDown',
+					timeOut: 3000
+				};
+				toastr.success('Rapat berhasil dihapus.');
+
+			@endif
+			
+			// delete confirmation
+			$('#modal-konfirmasi').on('show.bs.modal', function (event) {
+			var div = $(event.relatedTarget) // Tombol dimana modal di tampilkan
+			 
+				// Untuk mengambil nilai dari data-id="" yang telah kita tempatkan pada link hapus
+				var id = div.data('id')
+				 
+				var modal = $(this)
+				 
+				// Mengisi atribut href pada tombol ya yang kita berikan id hapus-true pada modal .
+				modal.find('#hapus-true').attr("href","{{ action('EditRapatController@getDelete') }}/"+id);
+			 
+			})	
 			
 			var mydata = <?php echo $result?>;
 				
-			function link(cellValue, options, rowdata, action)
+			function linkEdit(cellValue, options, rowdata, action)
 			{
-				return "<a href='" + rowdata.id + "'><span class='ui-icon ui-icon-folder-collapsed'>&nbsp;&nbsp;&nbsp;</span></a>";
+				return "<a href='{{ action('EditRapatController@getEdit') }}/" + rowdata.id + "'><span class='ui-icon ui-icon-pencil'>&nbsp;&nbsp;&nbsp;</span></a>";
 			}
 			
-			function link2(cellValue, options, rowdata, action)
+			function linkDelete(cellValue, options, rowdata, action)
 			{
-				return "<a href='" + rowdata.name + "'><span class='ui-icon ui-icon-circle-check'>&nbsp;&nbsp;&nbsp;&nbsp;</span></a>";
+				return "<a href='javascript:;' data-id='"+ rowdata.id +"' data-toggle='modal' data-target='#modal-konfirmasi'><span class='ui-icon ui-icon-trash'>&nbsp;&nbsp;&nbsp;&nbsp;</span></a>";
+			}
+			
+			
+			function konfirmasi_kehadiran(cellValue, options, rowdata, action)
+			{
+				return "<a href='{{action('PejabatController@getKonfirmasiKehadiran')}}?id="+rowdata.id+"'><span class='ui-icon ui-icon-note'>&nbsp;&nbsp;&nbsp;&nbsp;</span></a>";
+			}
+			
+			function laporan(cellValue, options, rowdata, action)
+			{
+				return "<a href='{{action('ReportController@getDetil')}}?id="+rowdata.id+"'><span class='ui-icon ui-icon-document'>&nbsp;&nbsp;&nbsp;&nbsp;</span></a>";
 			}
 
 			// Configuration for jqGrid Example 1
@@ -34,15 +89,17 @@
 				shrinkToFit: true,
 				rowNum: 14,
 				rowList: [10, 20, 30],
-				colNames: ['Jenis Rapat', 'Waktu', 'Tempat', 'Pembahasan', 'Pimpinan', 'Aksi 1', 'Aksi 2'],
-				colModel: [
-					{name: 'jenis_rapat', index: 'jenis_rapat', width: 90},
-					{name: 'waktu', index: 'waktu', width: 100, formatter: "date", formatoptions: { srcformat: "ISO8601Long", newformat: "m/d/Y H:i" }},
-					{name: 'tempat', index: 'tempat', width: 80},
-					{name: 'pembahasan', index: 'pembahasan', width: 80},
-					{name: 'pimpinan', index: 'pimpinan', width: 80 },
-					{name: 'aksi1', index: 'aksi1', width: 40,align: 'center',formatter: link},
-					{name: 'aksi1', index: 'aksi1', width: 40,align: 'center',formatter: link2}
+				colNames: ['Jenis Rapat', 'Waktu', 'Tempat', 'Pembahasan', 'Pimpinan', 'Konfirmasi Kehadiran', 'Laporan', 'Edit', 'Delete'],
+					colModel: [
+						{name: 'jenis_rapat', index: 'jenis_rapat', width: 90},
+						{name: 'waktu', index: 'waktu', width: 80, formatter: "date", formatoptions: { srcformat: "ISO8601Long", newformat: "m/d/Y H:i" }},
+						{name: 'tempat', index: 'tempat', width: 80},
+						{name: 'pembahasan', index: 'pembahasan', width: 80},
+						{name: 'pimpinan', index: 'pimpinan', width: 80 },
+						{name: 'konfirmasi_kehadiran', index: 'konfirmasi_kehadiran', width: 50,align: 'center',formatter: konfirmasi_kehadiran},
+						{name: 'laporan', index: 'laporan',width: 60,align: 'center',formatter: laporan},
+						{name: 'edit', index: 'edit', width: 20,align: 'center',formatter: linkEdit},
+						{name: 'delete', index: 'delete', width: 30,align: 'center',formatter: linkDelete}
 					
 				],
 				pager: "#pager_list_1",
@@ -106,6 +163,30 @@
 		
 		
 		
+	</div>
+</div>
+
+<!-- modal konfirmasi-->
+<div id="modal-konfirmasi" class="modal inmodal" tabindex="-1" role="dialog" aria-labelledby="myLargeModalLabel" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content animated fadeIn">
+		 
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h4 class="modal-title">Konfirmasi</h4>
+				<small>Menghapus data rapat</small>
+			</div>
+			 
+			<div class="modal-body">
+				<p>Apakah Anda yakin ingin menghapus data ini?</p>
+			</div>
+			 
+			<div class="modal-footer">
+				<a href="javascript:;" class="btn btn-white" id="hapus-true">Ya</a>
+				<button type="button" class="btn btn-primary" data-dismiss="modal">Tidak</button>
+			</div>
+		 
+		</div>
 	</div>
 </div>
 @endsection
