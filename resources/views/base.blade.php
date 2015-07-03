a093ca66342b26c96060d5a675460f417966913e
adwisatya <a.dwisaty4@yahoo.com>
2015-07-03 14:46:29 +0700

    Update navigasi daftar pejabat

--- resources/views/base.blade.php
+++ resources/views/base.blade.php
@@ -45,17 +45,17 @@
                 </li>
 
 				<li class="{{ isset($nav_pejabat) ? 'active' : '' }}">
-                    <a href="minor.html"><i class="fa fa-users"></i> <span class="nav-label">Daftar Pejabat</span></a>
-					<ul class="nav nav-second-level">
-						<li class="{{ isset($nav_polhukam) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','polhukam') }}">Polhukam</a></li>
-						<li class="{{ isset($nav_kemdagri) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kemdagri') }}">Kemdagri</a></li>
-						<li class="{{ isset($nav_kemlu) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kemlu') }}">Kemlu</a></li>
-						<li class="{{ isset($nav_kemhan) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kemhan') }}">Kemhan</a></li>
-						<li class="{{ isset($nav_kemenkumham) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kemenkumham') }}">Kemenkumham</a></li>
-						<li class="{{ isset($nav_kejagung) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kejagung') }}">Kejagung</a></li>
-						<li class="{{ isset($nav_mabestni) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','mabestni') }}">Mabes TNI</a></li>
-						<li class="{{ isset($nav_mabespolri) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','mabespolri') }}">Mabes Polri</a></li>
-					</ul>
+                    <a href="{{ action('PejabatController@getPejabat','all') }}"><i class="fa fa-users"></i> <span class="nav-label">Daftar Pejabat</span></a>
+					{{--<ul class="nav nav-second-level">--}}
+						{{--<li class="{{ isset($nav_polhukam) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','polhukam') }}">Polhukam</a></li>--}}
+						{{--<li class="{{ isset($nav_kemdagri) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kemdagri') }}">Kemdagri</a></li>--}}
+						{{--<li class="{{ isset($nav_kemlu) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kemlu') }}">Kemlu</a></li>--}}
+						{{--<li class="{{ isset($nav_kemhan) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kemhan') }}">Kemhan</a></li>--}}
+						{{--<li class="{{ isset($nav_kemenkumham) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kemenkumham') }}">Kemenkumham</a></li>--}}
+						{{--<li class="{{ isset($nav_kejagung) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','kejagung') }}">Kejagung</a></li>--}}
+						{{--<li class="{{ isset($nav_mabestni) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','mabestni') }}">Mabes TNI</a></li>--}}
+						{{--<li class="{{ isset($nav_mabespolri) ? 'active' : '' }}"><a href="{{ action('PejabatController@getPejabat','mabespolri') }}">Mabes Polri</a></li>--}}
+					{{--</ul>--}}
                 </li>
                 <li class="{{ isset($nav_entry) ? 'active' : '' }}">
                     <a href="{{ action('EntryController@getIndex') }}"><i class="fa fa-plus-square"></i> <span class="nav-label">Entry Rapat Baru</span></a>
